076fc4f24ca0a7d5c40648254a17a7ef5e552f8e
Steve Lai <steveplai@liontravel.com>
2026-05-15 12:41:06 +0800

    feat: add generic cross sell widget types

---
+++ src/widgets/cross-sell-widget/types.ts
@@ -0,0 +1,43 @@
+import type {
+  FlightOrderCrossSellAddon,
+  FlightOrderCrossSellAddonEvent,
+  FlightOrderCrossSellBenefit,
+  FlightOrderCrossSellCategory,
+  FlightOrderCrossSellContentOverrides,
+  FlightOrderCrossSellData,
+  FlightOrderCrossSellDefaultData,
+  FlightOrderCrossSellItem,
+  FlightOrderCrossSellItemEvent,
+  FlightOrderCrossSellOrder,
+  FlightOrderCrossSellPromo,
+  FlightOrderCrossSellProps,
+  FlightOrderCrossSellReminder,
+  FlightOrderCrossSellResolvedSection,
+  FlightOrderCrossSellSection,
+  FlightOrderCrossSellSectionContentOverrides,
+  FlightOrderCrossSellSectionContentOverridesByKind,
+  FlightOrderCrossSellSectionKind,
+  FlightOrderCrossSellViewMoreEvent,
+} from '../flight-order-cross-sell'
+
+export type CrossSellWidgetBenefit = FlightOrderCrossSellBenefit
+export type CrossSellWidgetPromo = FlightOrderCrossSellPromo
+export type CrossSellWidgetItem = FlightOrderCrossSellItem
+export type CrossSellWidgetSectionKind = FlightOrderCrossSellSectionKind
+export type CrossSellWidgetCategory = FlightOrderCrossSellCategory
+export type CrossSellWidgetSection = FlightOrderCrossSellSection
+export type CrossSellWidgetResolvedSection = FlightOrderCrossSellResolvedSection
+export type CrossSellWidgetSectionContentOverrides =
+  FlightOrderCrossSellSectionContentOverrides
+export type CrossSellWidgetSectionContentOverridesByKind =
+  FlightOrderCrossSellSectionContentOverridesByKind
+export type CrossSellWidgetReminder = FlightOrderCrossSellReminder
+export type CrossSellWidgetAddon = FlightOrderCrossSellAddon
+export type CrossSellWidgetOrder = FlightOrderCrossSellOrder
+export type CrossSellWidgetData = FlightOrderCrossSellData
+export type CrossSellWidgetDefaultData = FlightOrderCrossSellDefaultData
+export type CrossSellWidgetContentOverrides = FlightOrderCrossSellContentOverrides
+export type CrossSellWidgetItemEvent = FlightOrderCrossSellItemEvent
+export type CrossSellWidgetViewMoreEvent = FlightOrderCrossSellViewMoreEvent
+export type CrossSellWidgetAddonEvent = FlightOrderCrossSellAddonEvent
+export type CrossSellWidgetProps = FlightOrderCrossSellProps
