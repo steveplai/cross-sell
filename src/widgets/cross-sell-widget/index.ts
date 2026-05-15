f2c13d675779fb13f12a06daa0b9da2c6465cea1
Steve Lai <steveplai@liontravel.com>
2026-05-15 12:41:21 +0800

    feat: add generic cross sell widget exports

---
+++ src/widgets/cross-sell-widget/index.ts
@@ -0,0 +1,25 @@
+export {
+  FlightOrderCrossSell as CrossSellWidget,
+} from '../flight-order-cross-sell'
+
+export type {
+  CrossSellWidgetAddon,
+  CrossSellWidgetAddonEvent,
+  CrossSellWidgetBenefit,
+  CrossSellWidgetCategory,
+  CrossSellWidgetContentOverrides,
+  CrossSellWidgetData,
+  CrossSellWidgetDefaultData,
+  CrossSellWidgetItem,
+  CrossSellWidgetItemEvent,
+  CrossSellWidgetOrder,
+  CrossSellWidgetPromo,
+  CrossSellWidgetProps,
+  CrossSellWidgetReminder,
+  CrossSellWidgetResolvedSection,
+  CrossSellWidgetSection,
+  CrossSellWidgetSectionContentOverrides,
+  CrossSellWidgetSectionContentOverridesByKind,
+  CrossSellWidgetSectionKind,
+  CrossSellWidgetViewMoreEvent,
+} from './types'
